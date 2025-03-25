37dfef38bf9dd3bc1c4b56cc4bc2aacfd9e4d5e5
Alexey Kozadaev <akozadaev@skillcup.ru>
2025-03-26 00:29:59 +0300

    добавляет вторую версию API, полностью с авторизацией

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -53,6 +53,24 @@ func (h *Handler) getLink(c *gin.Context) {
 	})
 }
 
+func (h *Handler) getLinkByUser(c *gin.Context) {
+	handler.HandleRequest(c, h.cfg.ServerConfig.GoroutineTimeout, func(c *gin.Context) *handler.Response {
+		logger := logging.FromContext(c)
+		logger.Debugw("getting link", "get")
+		linkStr := c.Param("link")
+		res, err := h.shortenRepository.FindLink(c.Request.Context(), linkStr)
+		if err != nil {
+			return handler.NewInternalErrorResponse(err)
+		}
+
+		res.Shortened = fmt.Sprintf("%s%s", h.cfg.ServerConfig.Host, res.Shortened)
+		return handler.NewSuccessResponse(
+			http.StatusOK,
+			res,
+		)
+	})
+}
+
 func (h *Handler) getLinks(c *gin.Context) {
 	handler.HandleRequest(c, h.cfg.ServerConfig.GoroutineTimeout, func(c *gin.Context) *handler.Response {
 		logger := logging.FromContext(c)
@@ -155,6 +173,65 @@ func (h *Handler) createLink(c *gin.Context) {
 	})
 }
 
+func (h *Handler) createLinkByUser(c *gin.Context) {
+	handler.HandleRequest(c, h.cfg.ServerConfig.GoroutineTimeout, func(c *gin.Context) *handler.Response {
+		logger := logging.FromContext(c)
+		logger.Debugw("creating link", "create")
+
+		user, err := authentication.GetUser(c)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, "")
+			return handler.NewInternalErrorResponse(err)
+		}
+
+		links := make([]model.CreateLink, 0)
+		body, err := io.ReadAll(c.Request.Body)
+		if !helper.RequestHasJsonArray(body) {
+			var link model.CreateLink
+			err = json.Unmarshal(body, &link)
+			if err != nil {
+				_ = c.Error(err)
+				c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+				return handler.NewInternalErrorResponse(err)
+			}
+			links = append(links, link)
+		} else {
+			err = json.Unmarshal(body, &links)
+			if err != nil {
+				_ = c.Error(err)
+				c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+				return handler.NewInternalErrorResponse(err)
+			}
+		}
+		if err != nil {
+			c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+			return handler.NewInternalErrorResponse(err)
+		}
+
+		linkResponse := NewLinkResponse(links, h.cfg.ServerConfig.Host)
+
+		userArray := make([]*model.User, 0)
+		userArray = append(userArray, user)
+
+		for _, datum := range linkResponse.Data {
+			link := model.Link{
+				Source:    datum.Source,
+				Shortened: datum.Shortened,
+				User:      userArray,
+			}
+			err = h.shortenRepository.SaveLink(c.Request.Context(), &link)
+			if err != nil {
+				return handler.NewInternalErrorResponse(err)
+			}
+		}
+
+		return handler.NewSuccessResponse(
+			http.StatusOK,
+			linkResponse,
+		)
+	})
+}
+
 func RouteV1(cfg *config.Config, h *Handler, r *gin.Engine, auth authentication.Authentication) {
 	v1 := r.Group("v1")
 
@@ -169,7 +246,22 @@ func RouteV1(cfg *config.Config, h *Handler, r *gin.Engine, auth authentication.
 		v1.POST("/short", h.createLink)
 		v1.Use(authMiddleware).GET("/user/:id", h.getUser)
 		v1.Use(authMiddleware).GET("/users", h.getUsers)
-		//v1.POST("/short", h.createLink)
+	}
+}
+
+func RouteV2(cfg *config.Config, h *Handler, r *gin.Engine, auth authentication.Authentication) {
+	v2 := r.Group("v2")
+
+	client, err := trace.NewTraceClient()
+	authMiddleware := middleware.AuthenticationMiddleware(auth)
+	if err != nil {
+		log.Error().Stack().Err(err)
+	}
+	v2.Use(client.MiddleWareTrace())
+	{
+		v2.Use(authMiddleware).POST("/short", h.createLinkByUser)
+		v2.Use(authMiddleware).GET("/user/:id", h.getUser)
+		v2.Use(authMiddleware).GET("/users", h.getUsers)
 	}
 
 }
--- internal/app/model/links_model.go
+++ internal/app/model/links_model.go
@@ -6,9 +6,9 @@ import (
 
 type Link struct {
 	gorm.Model
-	Source    string `gorm:"column:source;type:varchar(2048);" json:"source"`
-	Shortened string `gorm:"column:shortened;type:varchar(256);" json:"shortened"`
-	User      []Link `gorm:"many2many:user_link;"`
+	Source    string  `gorm:"column:source;type:varchar(2048);" json:"source"`
+	Shortened string  `gorm:"column:shortened;type:varchar(256);" json:"shortened"`
+	User      []*User `gorm:"many2many:user_link;"`
 }
 
 type CreateLink struct {
