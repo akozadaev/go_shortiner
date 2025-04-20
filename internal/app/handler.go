7fee041bec7b30d84428a01264e073103917cdbc
Alexey Kozadaev <akozadaev@skillcup.ru>
2025-04-21 00:15:13 +0300

    добавляет постановку отложенной задачи на генерацию отчета

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -11,27 +11,33 @@ import (
 	"go_shurtiner/internal/http/handler"
 	"go_shurtiner/internal/http/helper"
 	"go_shurtiner/internal/http/middleware"
+	"go_shurtiner/internal/queue/service"
 	"go_shurtiner/pkg/config"
 	"go_shurtiner/pkg/logging"
 	trace "go_shurtiner/pkg/trace"
+	//"gopkg.in/guregu/null.v4"
 	"io"
 	"net/http"
+	//"time"
 )
 
 type Handler struct {
-	shortenRepository repository.ShortenRepository
-	userRepository    repository.UserRepository
-	cfg               *config.Config
+	shortenRepository  repository.ShortenRepository
+	userRepository     repository.UserRepository
+	queueJobRepository service.QueueRepository
+	cfg                *config.Config
 }
 
 func NewHandler(
 	shortenRepository repository.ShortenRepository,
 	userRepository repository.UserRepository,
+	queueJobRepository service.QueueRepository,
 	cfg *config.Config) *Handler {
 	return &Handler{
-		shortenRepository: shortenRepository,
-		userRepository:    userRepository,
-		cfg:               cfg,
+		shortenRepository:  shortenRepository,
+		userRepository:     userRepository,
+		queueJobRepository: queueJobRepository,
+		cfg:                cfg,
 	}
 }
 
@@ -232,61 +238,34 @@ func (h *Handler) createLinkByUser(c *gin.Context) {
 	})
 }
 
-func (h *Handler) setBackgroundTask(c *gin.Context) {
+func (h *Handler) setBackgroundTaskAsJob(c *gin.Context) {
 	handler.HandleRequest(c, h.cfg.ServerConfig.GoroutineTimeout, func(c *gin.Context) *handler.Response {
 		logger := logging.FromContext(c)
 		logger.Debugw("creating background task", "create")
 
-		user, err := authentication.GetUser(c)
+		_, err := authentication.GetUser(c)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, "")
 			return handler.NewInternalErrorResponse(err)
 		}
 
-		links := make([]model.CreateLink, 0)
+		queueJobs := make([]model.JobQueue, 0)
 		body, err := io.ReadAll(c.Request.Body)
 		if !helper.RequestHasJsonArray(body) {
-			var link model.CreateLink
-			err = json.Unmarshal(body, &link)
+			var queueJob model.JobQueue
+			err = json.Unmarshal(body, &queueJob)
 			if err != nil {
 				_ = c.Error(err)
 				c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 				return handler.NewInternalErrorResponse(err)
 			}
-			links = append(links, link)
-		} else {
-			err = json.Unmarshal(body, &links)
-			if err != nil {
-				_ = c.Error(err)
-				c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-				return handler.NewInternalErrorResponse(err)
-			}
-		}
-		if err != nil {
-			c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-			return handler.NewInternalErrorResponse(err)
-		}
-
-		linkResponse := NewLinkResponse(links, h.cfg.ServerConfig.Host)
-
-		userArray := make([]*model.User, 0)
-		userArray = append(userArray, user)
-
-		for _, datum := range linkResponse.Data {
-			link := model.Link{
-				Source:    datum.Source,
-				Shortened: datum.Shortened,
-				User:      userArray,
-			}
-			err = h.shortenRepository.SaveLink(c.Request.Context(), &link)
-			if err != nil {
-				return handler.NewInternalErrorResponse(err)
-			}
+			h.queueJobRepository.CreateJob(c, &queueJob)
+			queueJobs = append(queueJobs, queueJob)
 		}
 
 		return handler.NewSuccessResponse(
 			http.StatusOK,
-			linkResponse,
+			queueJobs,
 		)
 	})
 }
@@ -322,7 +301,7 @@ func RouteV2(cfg *config.Config, h *Handler, r *gin.Engine, auth authentication.
 		v2.POST("/short", h.createLinkByUser)
 		v2.GET("/user/:id", h.getUser)
 		v2.GET("/users", h.getUsers)
-		v2.POST("/report", h.setBackgroundTask)
+		v2.POST("/report", h.setBackgroundTaskAsJob)
 	}
 
 }
